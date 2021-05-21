b4c4ab23f84a33680b5a4326fd2c497d5496e39b
Oleksiy_Taranenko <oleksiytaranenko01@gmail.com>
2021-05-21 15:46:45 +0300

    homework 14

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function createButtons() {
+    for (let i = 0; i < 10; i++) {
+        let button = document.createElement('button');
+        button.textContent = `Button ${i}`;
+        button.addEventListener('click', function() {
+            console.log(`My number is ${i}`);
+        });
+
+        document.body.appendChild(button);
+    };
+};
+
+createButtons();
